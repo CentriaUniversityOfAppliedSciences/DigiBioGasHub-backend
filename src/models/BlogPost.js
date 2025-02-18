64b3e72d0ba9ec0895e92b3fdca32b869c280787
zprakash <prac303@gmail.com>
2025-02-18 14:10:38 +0200

    added postid

--- src/models/BlogPost.js
+++ src/models/BlogPost.js
@@ -1,9 +1,14 @@
 import { DataTypes } from 'sequelize';
-
+import crypto from 'crypto';
 import  sequelize  from './database.js';
 
 const BlogPost = sequelize.define('BlogPost',{
-
+    postID: {
+        type: DataTypes.STRING(10),
+        allowNull: false,
+        unique: true,
+        primaryKey: true
+    },
     title: {
         type:DataTypes.TEXT,
         allowNull:false
@@ -23,4 +28,10 @@ const BlogPost = sequelize.define('BlogPost',{
         type: DataTypes.SMALLINT,
     }
 });
+
+BlogPost.beforeValidate((post) => {
+    post.postID = crypto.randomBytes(5).toString('hex'); // Generates a 10-char string
+    console.log("Generated postID:", post.postID);
+});
+
 export default BlogPost;
