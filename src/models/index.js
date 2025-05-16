ddcd702aa659dacf5116c84b553b14a74f3a47df
Risto Hietala <risto.hietala@centria.fi>
2025-05-16 09:43:40 +0000

    fix for location table foreign key

--- src/models/index.js
+++ src/models/index.js
@@ -12,34 +12,18 @@ import BlogPost from './BlogPost.js';
 import Files from './files.js';
 import Subscription from './subscription.js';
 
-//User.hasMany(UserCompany, { foreignKey: 'userID' });
-//UserCompany.belongsTo(User, { foreignKey: 'id' });
 Company.belongsToMany(User, { through: UserCompany, foreignKey: 'companyID' });
 User.belongsToMany(Company, { through: UserCompany, foreignKey: 'userID' });
 
-//Company.hasMany(UserCompany, { foreignKey: 'companyID' });
-//UserCompany.belongsTo(Company, { foreignKey: 'id' });
-Company.hasMany(Location, { foreignKey: "parent"});
-//Company.hasMany(Offer, {foreignKey:"companyID"});
+Company.hasMany(Location, { foreignKey: "companyID" });
 Offer.belongsTo(Company, {foreignKey:"companyID"});
-//Material.hasMany(Offer,{foreignKey:"materialID"});
 Offer.belongsTo(Material, {foreignKey:"materialID"});
-//Location.hasMany(Offer,{foreignKey:"locationID"});
-//Offer.belongsTo(Location, {foreignKey:"id"});
-//User.hasMany(Offer,{foreignKey:"creator"});
 Offer.belongsTo(User, {foreignKey:"creator"});
-//Location.belongsTo(Company, { foreignKey: 'id' });
-//Company.hasMany(Location, { foreignKey: 'companyID' });
 Company.hasMany(Offer, { foreignKey:"companyID"});
 Contract.belongsTo(Offer, {foreignKey:"offerID"});
 Offer.hasMany(Contract, {foreignKey:"offerID"});
-//Offer.belongsTo(Company);
-
-Offer.hasMany(Files, { foreignKey: 'parent' })
-Offer.hasMany(Location, { foreignKey: 'parent' })
-
-
-
+Offer.hasMany(Files, { foreignKey: 'parent' });
+Offer.hasMany(Location, { foreignKey: 'parent' });
 
 User.hasMany(Subscription, { foreignKey: 'userID' });
 User.hasMany(BlogPost, { foreignKey: 'userID' });
