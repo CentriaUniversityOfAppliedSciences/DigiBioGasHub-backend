0e9399c22fd7a9fd01647d7c60229aa282b4e181
zprakash <prac303@gmail.com>
2025-08-19 09:41:31 +0300

    Add unpublish blog endpoint

--- src/routes/blog.js
+++ src/routes/blog.js
@@ -25,7 +25,8 @@ router.post("/sendforreview", async (req, res) => {
             blogPostType: 0
         }, {
             where: {
-                postID: body.postID
+                postID: body.postID,
+                userID: decoded.id
             },
             returning: true
         });
@@ -134,7 +135,8 @@ router.post("/updateblogpost", async (req, res) => {
             blogPostType: 2
         }, {
             where: {
-                postID: body.postID
+                postID: body.postID,
+                userID: decoded.id
             },
             returning: true
         });
@@ -166,6 +168,64 @@ router.post("/updateblogpost", async (req, res) => {
         res.status(500).json({ "type": "result", "result": "fail", "message": "unable to update blog post" });
     }
 });
+
+/*
+* @route POST /blog/unpublishreq
+* @param {uuid} postID
+* @return {json} 
+  * @key type @value result
+  * @key result @value ["ok", "fail"]
+*/
+router.post("/unpublishreq", async (req, res) => {
+
+    const token = req.headers['authorization'];
+    var [result, decoded] = await secTest(token);
+
+    if (!result) {
+        return res.status(401).json({ "type": "result", "result": "fail", "message": "Unauthorized access" });
+    }
+
+    try {
+        var body = req.body;
+        const [numberOfAffectedRows, blogpost] = await BlogPost.update({
+            blogPostType: 2
+        }, {
+            where: {
+                postID: body.postID,
+                userID: decoded.id
+            },
+            returning: true
+        });
+        if (numberOfAffectedRows > 0) {
+            Logs.create({
+                userID: decoded.id,
+                action: req.url,
+                text: "Blog post unpublished: " + body.postID + " from ip:" + req.ip,
+                level: 1
+            });
+            res.json({ "type": "result", "result": "ok" });
+        } else {
+            Logs.create({
+                userID: decoded.id,
+                action: req.url,
+                text: "Blog post not found: " + body.postID + " from ip:" + req.ip,
+                level: 1
+            });
+            res.status(404).json({ "type": "result", "result": "fail", "message": "Blog post not found" });
+        }
+    } catch (error) {
+        console.error(error);
+        Logs.create({
+            userID: null,
+            action: req.url,
+            text: "error: " + error + " from ip:" + req.ip,
+            level: 3
+        });
+        res.status(500).json({ "type": "result", "result": "fail", "message": "unable to change blog post status" });
+    }
+});
+
+
 
 export default router;
 
