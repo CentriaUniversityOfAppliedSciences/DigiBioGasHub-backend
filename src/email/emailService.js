471b2545569ae46d0598677ff330c91f78286960
zprakash <prac303@gmail.com>
2025-10-17 13:12:52 +0300

    Refactor email sending logic to use Azure Communication Services

--- src/email/emailService.js
+++ src/email/emailService.js
@@ -1,36 +1,35 @@
-import { createTransport } from 'nodemailer';
-function sendEmail(receiver, subject, msgHTML, callback)
-{
-    // Create the transporter with the required configuration for Outlook
-    // change the user and pass !
-    var transporter = createTransport({
-        host: "smtp-mail.outlook.com", // hostname
-        secureConnection: false, // TLS requires secureConnection to be false
-        port: 587, // port for secure SMTP
-        tls: {
-           ciphers:'SSLv3'
-        },
-        auth: {
-            user: process.env.EMAIL_SENDER, // sender address (who sends)
-            pass: process.env.EMAIL_PASS
-        }
-    });
-    // setup e-mail data, even with unicode symbols
-    var mailOptions = {
-        from: process.env.EMAIL_SENDER, // sender address (who sends)
-        to: receiver, // list of receivers (who receives)
-        subject: subject, // Subject line
-        text: '', // plaintext body
-        html: msgHTML // html body
-    };
-    // send mail with defined transport object
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            callback(false, error);
-        } else {
+import { EmailClient } from "@azure/communication-email";
+
+const connectionString = process.env.EMAIL_CONNECTION_STRING;
+const senderAddress = process.env.EMAIL_SENDER;
+
+const client = new EmailClient(connectionString);
+
+async function sendEmail(receiver, subject, msgHTML, callback) {
+    try {
+        const emailMessage = {
+            senderAddress: senderAddress,
+            content: {
+                subject: subject,
+                plainText: msgHTML.replace(/<[^>]+>/g, ''), 
+                html: msgHTML,
+            },
+            recipients: {
+                to: [{ address: receiver }],
+            },
+        };
+
+        const poller = await client.beginSend(emailMessage);
+        const result = await poller.pollUntilDone();
+
+        if (result.status === "Succeeded") {
             callback(true);
+        } else {
+            callback(false, result.error || "Email send failed");
         }
-    });
+    } catch (error) {
+        callback(false, error);
+    }
 }
 
 export default sendEmail;
